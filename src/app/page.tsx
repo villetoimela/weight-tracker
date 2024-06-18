dc5448d873cbbe350c51789f08fa3c765c8ead09
Ville Toimela <ville.toimela@bermuda.fi>
2024-06-18 12:51:44 +0300

    Added jsonwebtoken cookie and refactor code to work with it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import RegisterForm from '../components/RegisterForm';
 import LoginForm from '../components/LoginForm';
 import WeightEntryForm from '../components/WeightEntryForm';
 import WeightList from '../components/WeightList';
+import axios from 'axios';
 
 export default function Home() {
   const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      try {
+        const response = await axios.get('/api/checkAuth');
+        setUserId(response.data.userId);
+      } catch (error) {
+        setUserId(null);
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
 
   return (
     <div>
--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,5 +1,9 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { serialize } from 'cookie';
 import { openDB } from '../../../lib/db.mjs';
+
+const SECRET_KEY = process.env.JWT_SECRET;
 
 export default async (req, res) => {
   if (req.method === 'POST') {
@@ -9,6 +13,13 @@ export default async (req, res) => {
     const user = await db.get('SELECT * FROM Users WHERE username = ?', [username]);
 
     if (user && await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ userId: user.id }, SECRET_KEY, { expiresIn: '1h' });
+      res.setHeader('Set-Cookie', serialize('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        maxAge: 3600,
+        path: '/',
+      }));
       res.status(200).json({ message: 'Login successful', userId: user.id });
     } else {
       res.status(401).json({ message: 'Invalid username or password' });
