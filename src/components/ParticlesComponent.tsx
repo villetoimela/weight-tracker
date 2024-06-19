e71f5a3b92949865f73dd184c8c4005140da0457
Ville Toimela <ville.toimela@bermuda.fi>
2024-06-19 13:51:07 +0300

    Added particlesjs and make random background with it

---
+++ src/components/ParticlesComponent.tsx
@@ -0,0 +1,31 @@
+// src/components/ParticlesComponent.tsx
+import React, { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+import type { Engine } from "tsparticles-engine";
+import particlesConfig from "../../public/particlesjs-config.json";
+
+const ParticlesComponent: React.FC = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={particlesConfig}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: -10,
+        backgroundColor: "#141414",
+      }}
+    />
+  );
+};
+
+export default ParticlesComponent;
